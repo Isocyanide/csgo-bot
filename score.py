7cb4eb261fc0c6d0d1f0aa67ff712c2cca1202ee
WeirdIndianBoi <42083405+WeirdIndianBoi@users.noreply.github.com>
2019-08-29 16:40:58 +0530

    Changed to link to current Majors Legends stage (Starladder Berlin)

--- score.py
+++ score.py
@@ -2,26 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-'''
-data = requests.get("http://esportlivescore.com/e_444767-g2-vs-tyloo-iem-katowice-2019-main-qualifier.html", headers = headers)
-
-soup = BeautifulSoup(data.content, features="html.parser")
-
-team_list = []
-
-teams = soup.find_all('div', class_= 'team-info__team')
-for team in teams:
-	team_list.append(team.find('h2').text)
-score_1 = soup.find('span', class_= 'team_1_overall_score_1').text
-score_2 = soup.find('span', class_= 'team_2_overall_score_1').text
-
-score_1 = f"{score_1} : {team_list[0]}"
-score_2 = f"{score_2} : {team_list[1]}"
-#print(soup.find_all('div', class_= 'teamName'))
-'''
 
 def points_scraper():
-	data = requests.get("https://www.hltv.org/events/3883/iem-katowice-2019", headers = headers)
+	data = requests.get("https://www.hltv.org/events/4443/starladder-major-2019", headers = headers)
 
 	soup = BeautifulSoup(data.content, features="html.parser")
 
